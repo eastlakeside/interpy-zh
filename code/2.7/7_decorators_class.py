c2509f0d06e471495383835c9421e70eca39ff71
suqi <deqi.su@gmail.com>
2016-04-06 10:50:12 +0800

    fix(code) 同步#33 的代码修复

--- code/2.7/7_decorators_class.py
+++ code/2.7/7_decorators_class.py
@@ -1,15 +1,24 @@
-class logit(object):
-	def __init__(self, logfile='out.log'):
-		self.logfile = logfile
-	def __call__(self, func):
-		log_string = func.__name__ + ' was called'
-		print log_string
+from functools import wraps
 
-		with open(self.logfile, 'a') as opened_file:
-			opened_file.write(log_string + '\n')
-		self.notify
-	def notify(self):
-		pass
+class logit(object):
+    def __init__(self, logfile='out.log'):
+        self.logfile = logfile
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            log_string = func.__name__ + " was called"
+            print(log_string)
+
+            with open(self.logfile, 'a') as opened_file:
+                opened_file.write(log_string + '\n')
+            self.notify()
+            
+            return func(*args, **kwargs)
+        return wrapped_function
+
+    def notify(self):
+        pass
 
 class email_logit(logit):
 	def __init__(self, email='admin@myproject.com', *args, **kwargs):
